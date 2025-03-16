a5db7401f0bb5da8ee018418b7d70cdf4e7c19a1
JuMaxi <jmaximovski@gmail.com>
2025-03-16 16:05:40 +0000

    Arrays and Hashings
    Solution to Leet Code Problem - Valid Anagram - Checking  the two input (strings) length, if it is different, returning false. Otherwise,  creating a dictionary, iterating throught the string1, storing the current char in the dictionary as a key, and value 1. If the dict already contains de key, just add 1 to current key. Then, iterating throught the string2, checking it the dict doesn't contains  the current key, if so, returning false, otherwise, diminish 1 to the dict current key. In the last iteration checking if the dict contais value different than 0, if so, returning false, otherwise, return true. Solution validated with Unit Tests.

---
+++ Data-Structures/Data-Structures/ArraysAndHashings/ValidAnagram.cs
@@ -0,0 +1,48 @@
+﻿namespace Data_Structures.ArraysAndHashings
+{
+    public class ValidAnagram
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if(s.Length != t.Length)
+            {
+                return false;
+            }
+
+            Dictionary<char, int> dic = new();
+
+            foreach(char c in s)
+            {
+                if (dic.ContainsKey(c))
+                {
+                    dic[c] += 1;
+                }
+                else
+                {
+                    dic.Add(c, 1);
+                }
+            }
+
+            foreach(char c in t)
+            {
+                if (!dic.ContainsKey(c))
+                {
+                    return false;
+                }
+                else
+                {
+                    dic[c] -= 1;
+                }
+            }
+
+            foreach(KeyValuePair<char, int> myDict in dic)
+            {
+                if (dic[myDict.Key] != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Data-Structures/Data-Structures/ArraysAndHashings/ValidAnagramTest.cs
@@ -0,0 +1,44 @@
+﻿namespace Data_Structures.ArraysAndHashings
+{
+    public class ValidAnagramTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            string s = "anagram";
+            string t = "nagaram";
+
+            ValidAnagram valid = new();
+
+            bool result = valid.IsAnagram(s, t);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            string s = "rat";
+            string t = "car";
+
+            ValidAnagram valid = new();
+
+            bool result = valid.IsAnagram(s, t);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            string s = "banana";
+            string t = "bbnana";
+
+            ValidAnagram valid = new();
+
+            bool result = valid.IsAnagram(s, t);
+
+            Assert.False(result);
+        }
+    }
+}
